d5cce50d09d663f9939d347846c466692cd9dbfb
Christerle_Nounoute <christerlesn@gmail.com>
2021-08-29 00:00:05 -0700

    Idea model, migration, and association

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 social_media = Category.create(name: "Social Media")
 gaming = Category.create(name: "Gaming")
 entertainment = Category.create(name: "Entertainment")
+
+Idea.create(title: "Fakebook", description: "A site where catfishers can live in peace and harmony.", category_id: social_media.id)
+Idea.create(title:"Flappy Fish", description: "A comeback for a certain game, but with fish instead.", category_id: gaming.id)
