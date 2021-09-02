ce24224d87fb390a7d8d3abea45ecd4552a7a64e
Christerle_Nounoute <christerlesn@gmail.com>
2021-09-01 19:14:22 -0700

    Added validations, seed, and dependancies for destroy

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,4 @@
 class Idea < ApplicationRecord
   belongs_to :category
+  validates :title, :description, :category_id presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,7 @@ entertainment = Category.create(name: "Entertainment")
 
 Idea.create(title: "Fakebook", description: "A site where catfishers can live in peace and harmony.", category_id: social_media.id)
 Idea.create(title:"Flappy Fish", description: "A comeback for a certain game, but with fish instead.", category_id: gaming.id)
+Idea.create(title: "Nextclip", description: "Clips of movies that you'll never watch", category_id: entertainment.id)
+Idea.create(title:"Twiiter", description: "Where birds and other vertebrates can chat in peace", category_id: social_media.id)
+Idea.create(title: "Fakebook_2", description: "In case the first one doesn't work", category_id: social_media.id)
+Idea.create(title:"Angry Fish", description: "A fish on a mission", category_id: gaming.id)
